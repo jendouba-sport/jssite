cb2ec9ee50eceb0ed365a169774a8cc9fb48797b
dhiaayadi1254 <adhia8115@gmail.com>
2025-10-04 14:58:12 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/jendouba-sport.github.io/', 
+  
   plugins: [react(), tailwindcss()],
 })
